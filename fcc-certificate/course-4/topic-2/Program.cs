39aca9964483797d00a469c697f201634c636014
cvazcosta <cvazcosta2001@gmail.com>
2025-09-30 20:44:07 -0300

    Adicionando tópico sobre switch case

---
+++ fcc-certificate/course-4/topic-2/Program.cs
@@ -0,0 +1,51 @@
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+using System.Net;
+
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
+{
+  case "01":
+    type = "Sweat shirt";
+    break;
+  case "02":
+    type = "T-Shirt";
+    break;
+  case "03":
+    type = "Sweat pants";
+    break;
+  default:
+    type = "Other";
+    break;
+}
+
+switch (product[1])
+{
+  case "BL":
+    color = "Black";
+    break;
+  case "MN":
+    color = "Maroon";
+    break;
+  default:
+    color = "White";
+    break;
+}
+
+// Switch Expression forma alternativa e condensada de usar Switch em C#
+size = product[2] switch
+{
+  "S" => "Small",
+  "M" => "Medium",
+  "L" => "Large",
+  _ => "One Size Fits All",
+};
+
+Console.WriteLine($"Product: {size} {color} {type}");
