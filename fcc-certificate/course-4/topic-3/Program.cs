be044bf6fc9b9bd3fee872f57f73cfe8b448e4ba
cvazcosta <cvazcosta2001@gmail.com>
2025-10-06 20:20:38 -0300

    Adicionando tópico sobre o loop for

---
+++ fcc-certificate/course-4/topic-3/Program.cs
@@ -0,0 +1,31 @@
+﻿// for (int i = 0; i < 10; i += 3)
+// {
+//   Console.WriteLine(i);
+// }
+
+// for (int i = 0; i < 10; i++)
+// {
+//   Console.WriteLine(i);
+//   if (i == 7) break;
+// }
+
+// string[] names = ["Alex", "Eddie", "David", "Michael"];
+// for (int i = names.Length - 1; i >= 0; i--)
+// {
+//   Console.WriteLine(names[i]);
+// }
+
+// Exemplo que mostra uma limitação do foreach
+// string[] names = ["Alex", "Eddie", "David", "Michael"];
+// foreach (var name in names)
+// {
+//     // Can't do this:
+//     if (name == "David") name = "Sammy";
+// }
+
+// Maneira de contornar a limitação acima
+string[] names = ["Alex", "Eddie", "David", "Michael"];
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
---
+++ fcc-certificate/course-4/topic-4/notas.md
@@ -0,0 +1,38 @@
+# Code challenge - implement the FizzBuzz challenge rules
+
+Here are the FizzBuzz rules that you need to implement in your code project:
+
+- Output values from 1 to 100, one number per line, inside the code block of an iteration statement.
+- When the current value is divisible by 3, print the term Fizz next to the number.
+- When the current value is divisible by 5, print the term Buzz next to the number.
+- When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.
+
+## Exemplo de saída
+
+```Plain Text
+1
+2
+3 - Fizz
+4
+5 - Buzz
+6 - Fizz
+7
+8
+9 - Fizz
+10 - Buzz
+11
+12 - Fizz
+13
+14
+15 - FizzBuzz
+16
+17
+18 - Fizz
+19
+20 - Buzz
+21 - Fizz
+22
+.
+.
+.
+```
