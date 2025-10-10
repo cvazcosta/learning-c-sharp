87197ec4f746f51a7e10ddd20b5ee14a65bf3665
cvazcosta <cvazcosta2001@gmail.com>
2025-10-09 21:35:37 -0300

    Resolução parcial do 2º exercício

---
+++ fcc-certificate/course-5/topic-4/Program.cs
@@ -0,0 +1,29 @@
+﻿string? userInput;
+bool validInput = false;
+
+Console.WriteLine("Inform your role:");
+Console.WriteLine("- Administrator");
+Console.WriteLine("- Manager");
+Console.WriteLine("- User");
+
+userInput = Console.ReadLine();
+
+if (userInput != null)
+{
+  // userInput.ToLower();
+  Console.WriteLine(userInput.Trim().ToLower());
+}
+
+Console.WriteLine(userInput);
+
+// while (validInput == false)
+// {
+//   userInput = Console.ReadLine();
+
+// }
+
+// Estou pensando em usar um switch dentro do while tendo como valor de comparação a entrada do usuário "limpa" pelo uso do Trim() e do ToLower()
+
+// Se a entrada do usuário for validada com sucesso, saímos do loop e imprimimos a mensagem de sucesso
+
+// Implementar em breve
