5eca24e1e3195ebf458e2377ef5fd254d424b7fc
cvazcosta <cvazcosta2001@gmail.com>
2025-10-15 16:28:45 -0300

    Resolve code exercise 3

--- fcc-certificate/course-5/topic-5/Program.cs
+++ fcc-certificate/course-5/topic-5/Program.cs
@@ -1,23 +1,23 @@
-﻿// string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
+﻿string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
 
-// periodLocation será usada p/ armazenar a posição do ponto em uma string
-// int periodLocation = 0;
+int periodLocation;
+string myString;
 
-// foreach (string myString in myStrings)
-// {
+for (int i = 0; i < myStrings.Length; i++)
+{
+  myString = myStrings[i];
+  do
+  {
+    periodLocation = myString.IndexOf('.');
 
-// }
+    if (periodLocation != -1)
+    {
+      Console.WriteLine(myString.Remove(periodLocation));
+      myString = myString.Substring(periodLocation + 1).TrimStart();
+      continue;
+    }
 
-string someSentence = "I like pizza. I like roast chicken. I like salad";
-
-// Quando periodLocation != -1
-int periodLocation = someSentence.IndexOf('.');
-Console.WriteLine(someSentence.Remove(periodLocation));
-someSentence = someSentence.Substring(periodLocation + 1).TrimStart();
-
-periodLocation = someSentence.IndexOf('.');
-Console.WriteLine(someSentence.Remove(periodLocation));
-someSentence = someSentence.Substring(periodLocation + 1).TrimStart();
-
-// Quando periodLocation == -1
-Console.WriteLine(someSentence);
+    Console.WriteLine(myString);
+      
+  } while (periodLocation != -1);
+}
