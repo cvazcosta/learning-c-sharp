154e79d44536d12319c2451cc6a5676d9fe5e40c
cvazcosta <cvazcosta2001@gmail.com>
2025-10-14 21:32:17 -0300

    Resolução parcial do code challenge 3

--- fcc-certificate/course-5/topic-5/Program.cs
+++ fcc-certificate/course-5/topic-5/Program.cs
@@ -1,2 +1,23 @@
-﻿string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
+﻿// string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
 
+// periodLocation será usada p/ armazenar a posição do ponto em uma string
+// int periodLocation = 0;
+
+// foreach (string myString in myStrings)
+// {
+
+// }
+
+string someSentence = "I like pizza. I like roast chicken. I like salad";
+
+// Quando periodLocation != -1
+int periodLocation = someSentence.IndexOf('.');
+Console.WriteLine(someSentence.Remove(periodLocation));
+someSentence = someSentence.Substring(periodLocation + 1).TrimStart();
+
+periodLocation = someSentence.IndexOf('.');
+Console.WriteLine(someSentence.Remove(periodLocation));
+someSentence = someSentence.Substring(periodLocation + 1).TrimStart();
+
+// Quando periodLocation == -1
+Console.WriteLine(someSentence);
