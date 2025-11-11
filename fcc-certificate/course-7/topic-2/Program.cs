9f4a9c8e485a79bf3c4e50928be8ebe76d8acaea
cvazcosta <cvazcosta2001@gmail.com>
2025-11-11 16:14:13 -0300

    Removendo comentários para facilitar leitura

--- fcc-certificate/course-7/topic-2/Program.cs
+++ fcc-certificate/course-7/topic-2/Program.cs
@@ -1,28 +1,28 @@
 ﻿using System.Globalization;
 
 // Definindo a cultura padrão do programa para en-US
-// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
-// CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 
-// int invoiceNumber = 1201;
-// decimal productShares = 25.4568m;
-// decimal subtotal = 2750.00m;
-// decimal taxPercentage = .15825m;
-// decimal total = 3185.19m;
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
 
-// Console.WriteLine($"Invoice Number: {invoiceNumber}");
-// Console.WriteLine($"\tShares: {productShares:N3} Product");
-// Console.WriteLine($"\t\tSub Total: {subtotal:C}");
-// Console.WriteLine($"\t\tTax: {taxPercentage:P2}");
-// Console.WriteLine($"\t\tTotal Billed:: {total:C}");
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"\tShares: {productShares:N3} Product");
+Console.WriteLine($"\t\tSub Total: {subtotal:C}");
+Console.WriteLine($"\t\tTax: {taxPercentage:P2}");
+Console.WriteLine($"\t\tTotal Billed:: {total:C}");
 
 // Aprendendo sobre padding
-// string input = "Pad this";
-// Console.WriteLine(input.PadRight(12));
+string input = "Pad this";
+Console.WriteLine(input.PadRight(12));
 
 // Outro overload do Pad
-// Console.WriteLine(input.PadLeft(24, '-'));
-// Console.WriteLine(input.PadRight(24, '-'));
+Console.WriteLine(input.PadLeft(24, '-'));
+Console.WriteLine(input.PadRight(24, '-'));
 
 // Trabalhando com padded strings
 string paymentId = "769C";
