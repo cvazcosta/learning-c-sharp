1bbce77e6d05a76b5c13977ffcfcdd107dc3dcb4
cvazcosta <cvazcosta2001@gmail.com>
2025-11-12 13:52:08 -0300

    Aprendendo outros helper methods para string

---
+++ fcc-certificate/course-8/topic-1/Program.cs
@@ -0,0 +1,47 @@
+﻿string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+openingPosition++;
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+
+// Segundo exemplo
+string message2 = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+int firstAngleBracketPosition = message2.IndexOf('<');
+int lastAngleBracketPosition = message2.LastIndexOf('>');
+int openingPosition2 = message2.IndexOf("<span>");
+int closingPosition2 = message2.IndexOf("</span>");
+
+// Verificando qual o valor de índice retornado quando uma string é passada para o IndexOf()
+Console.WriteLine($"first angle bracket: {firstAngleBracketPosition}");
+Console.WriteLine($"last angle bracket: {lastAngleBracketPosition}");
+Console.WriteLine($"openingPosition2: {openingPosition2}");
+Console.WriteLine($"closingPosition2: {closingPosition2}");
+
+openingPosition2 += openSpan.Length;
+
+int length2 = closingPosition2 - openingPosition2;
+Console.WriteLine(message2.Substring(openingPosition2, length2));
+
+// Terceira unidade
+string message3 = "hello there!";
+
+int first_h = message3.IndexOf('h');
+int last_h = message3.LastIndexOf('h');
+
+Console.WriteLine($"For the message: '{message3}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}");
+
+// Exercício
+string message4 = "(What if) I am (only interested) in the last (set of parentheses)?";
+int openingPosition4 = message.LastIndexOf('(');
+
+openingPosition4 += 1;
+int closingPosition4 = message.LastIndexOf(')');
+int length4 = closingPosition4 - openingPosition4;
+Console.WriteLine(message4.Substring(openingPosition4, length4));
---
+++ fcc-certificate/course-8/topic-1/notas.md
@@ -0,0 +1,3 @@
+# Reference
+
+Based on this [topic](https://learn.microsoft.com/en-us/training/paths/get-started-c-sharp-part-4/)
