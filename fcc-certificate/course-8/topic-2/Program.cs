77d2bccd7000e59c1f066ff4c605c674ff0e73f2
cvazcosta <cvazcosta2001@gmail.com>
2025-11-13 16:48:32 -0300

    Resolvendo exercício parcialmente

--- fcc-certificate/course-8/topic-2/Program.cs
+++ fcc-certificate/course-8/topic-2/Program.cs
@@ -99,3 +99,39 @@
 
 // =====================================================================
 // Unit 4 - Remove() and Replace()
+// string data = "12345John Smith          5000  3  ";
+// string updatedData = data.Remove(5, 20);
+// Console.WriteLine(updatedData);
+
+// string message = "This--is--ex-amp-le--da-ta";
+// message = message.Replace("--", " ");
+// message = message.Replace("-", "");
+// Console.WriteLine(message);
+// =====================================================================
+// Unit 5 - Exercise
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+
+const string openSpan = "<span>";
+
+int openingPosition = input.IndexOf("<span>"); 
+int closingPosition = input.IndexOf("</span>");
+
+Console.WriteLine($"openingPosition: {openingPosition}");
+Console.WriteLine($"closingPosition: {closingPosition}");
+
+// Necessário para que a posição inicial parta do índice imediatamente após <span>
+openingPosition += openSpan.Length;
+Console.WriteLine($"openingPosition after adding <span> length: {openingPosition}");
+
+int length = closingPosition - openingPosition;
+Console.WriteLine($"length: {length}");
+
+quantity = input.Substring(openingPosition, length);
+
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
---
+++ fcc-certificate/course-8/topic-2/notas.md
@@ -0,0 +1,23 @@
+# Exercise
+
+## Desired output
+
+```Output
+Quantity: 5000
+Output: <h2>Widgets &reg;</h2><span>5000</span>
+```
+
+Código HTML:
+
+```HTML
+<h2>Widgets &reg;</h2>
+<span>5000</span>
+```
+
+## Instructions
+
+- Set the quantity variable to the value obtained by extracting the text between the `<span>` and `</span>` tags.
+
+- Set the output variable to the value of input, then remove the `<div>` and `</div>` tags.
+
+- Replace the HTML character ™ (&trade;) with ® (&reg;) in the output variable.
